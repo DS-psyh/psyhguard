0c9fadf7241d674d391fd35da6a2fd649ca3e0b4
DS-psyh <ignatov9777@gmail.com>
2025-08-14 19:20:59 +0300

    Добавил проект

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE='psyhguard-cache-v1';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  e.respondWith(
+    caches.match(req).then(res=>res||fetch(req).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+      return r;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
